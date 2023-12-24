fce658ae85df24a58af74f3cf2f2284c4e659cfb
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-23 20:17:05 -0500

    Base commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer");
+
+const mainChoices = [
+"View All Employees",
+"Add Employee",
+"Update Employee Role",
+"View All Roles",
+"Add Role",
+"View All Departments",
+"Add Department",
+"Quit"
+]
+
+const promptUser = () => {
+    return inquirer.prompt([
+{
+    type:"list",
+    name: "main",
+    message: "What would you like to do?",
+    choices: mainChoices
+
+}
+    ]);
+};
+
+promptUser();
