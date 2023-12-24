9d7a828090934787d457be5cd24c00de939744c2
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-23 20:24:20 -0500

    document formatted

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const inquirer = require("inquirer");
 
 const mainChoices = [
-"View All Employees",
-"Add Employee",
-"Update Employee Role",
-"View All Roles",
-"Add Role",
-"View All Departments",
-"Add Department",
-"Quit"
-]
+  "View All Employees",
+  "Add Employee",
+  "Update Employee Role",
+  "View All Roles",
+  "Add Role",
+  "View All Departments",
+  "Add Department",
+  "Quit",
+];
 
 const promptUser = () => {
-    return inquirer.prompt([
-{
-    type:"list",
-    name: "main",
-    message: "What would you like to do?",
-    choices: mainChoices
-
-}
-    ]);
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "main",
+      message: `What would you like to do?`,
+      choices: mainChoices,
+    },
+  ]);
 };
 
 promptUser();
