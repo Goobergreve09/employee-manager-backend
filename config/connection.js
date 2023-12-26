65eb3ce7617eed1230469e35706f95e1fe57e0ce
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-26 15:07:39 -0500

    function inquirer

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,14 @@
 const mysql = require('mysql2');
 
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
 
-  const tables = ['Departments', 'Role', 'Employee'];
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Butterypopcorn94*",
+  database: "employee_manager_db"
+});
 
-  // Loop through each table and execute a SELECT query
-  tables.forEach((table) => {
-    const query = `SELECT * FROM ${table}`;
-    connection.query(query, function (err, results, fields) {
-      if (err) {
-        console.error(`Error retrieving data from ${table}: ${err.message}`);
-      } else {
-        console.log(`Data from ${table}:`);
-        console.log(results);
-      }
-    });
-  });
+const promisePool = pool.promise();
+
+module.exports = promisePool;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-
+const promisePool = require('./config/connection');
 
 
 const mainChoices = [
@@ -14,8 +14,8 @@ const mainChoices = [
 ];
 
 
-const promptUser = () => {
-  return inquirer.prompt([
+const promptUser = async () => {
+  const answers = await inquirer.prompt([
     {
       type: "list",
       name: "main",
@@ -23,15 +23,61 @@ const promptUser = () => {
       choices: mainChoices,
     },
   ]);
+
+  switch (answers.main) {
+    case "View All Employees":
+      await viewAllEmployees();
+      break;
+
+    case "Add Employee":
+      await addEmployee();
+      break;
+
+    case "Update Employee Role":
+      await updateEmployeeRole();
+      break;
+
+    case "View All Roles":
+      await viewAllRoles();
+      break;
+
+    case "Add Role":
+      await addRole();
+      break;
+
+    case "View All Departments":
+      await viewAllDepartments();
+      break;
+
+    case "Add Department":
+      await addDepartment();
+      break;
+
+    case "Quit":
+      console.log("Quitting...");
+      // You may want to close the connection here if necessary
+      break;
+
+    default:
+      console.log("Invalid choice");
+  }
 };
 
-
+const viewAllDepartments = async () => {
+  try {
+    const [rows, fields] = await promisePool.query('SELECT * FROM Departments');
+    console.table(rows);
+  } catch (error) {
+    console.error('Error fetching departments:', error.message);
+  }
+  // Prompt user again after displaying the table
+  await promptUser();
+};
+// Initial call to start the application
 
 promptUser();
 
-if (mainChoices === 'View All Departments') {
-    viewDepartments();
-  }
+
 
 
 
