20c8009f9db13460dd709a61a7634f6f71faa573
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-27 10:40:00 -0500

    update gitignore

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
-const promisePool = require('./config/connection');
-
+const promisePool = require("./config/connection");
 
 const mainChoices = [
   "View All Employees",
@@ -12,7 +11,6 @@ const mainChoices = [
   "Add Department",
   "Quit",
 ];
-
 
 const promptUser = async () => {
   const answers = await inquirer.prompt([
@@ -65,10 +63,10 @@ const promptUser = async () => {
 
 const viewAllDepartments = async () => {
   try {
-    const [rows, fields] = await promisePool.query('SELECT * FROM Departments');
+    const [rows, fields] = await promisePool.query("SELECT * FROM Departments");
     console.table(rows);
   } catch (error) {
-    console.error('Error fetching departments:', error.message);
+    console.error("Error fetching departments:", error.message);
   }
   // Prompt user again after displaying the table
   await promptUser();
@@ -84,7 +82,7 @@ async function viewAllRoles() {
     `);
     console.table(rows);
   } catch (error) {
-    console.error('Error fetching roles:', error.message);
+    console.error("Error fetching roles:", error.message);
   } finally {
     // Prompt user again or end the connection
     promptUser();
@@ -111,7 +109,7 @@ async function viewAllEmployees() {
 
     console.table(rows);
   } catch (error) {
-    console.error('Error fetching employees:', error.message);
+    console.error("Error fetching employees:", error.message);
   } finally {
     // Prompt user again or end the connection
     promptUser();
@@ -121,11 +119,15 @@ async function viewAllEmployees() {
 async function addEmployee() {
   try {
     // Fetch roles and departments
-    const [roles] = await promisePool.query('SELECT id, Title FROM Roles');
-    const [departments] = await promisePool.query('SELECT id, name FROM Departments');
-    const [managers] = await promisePool.query('SELECT id, CONCAT(first_name, " ", last_name) AS manager_name FROM Employee WHERE manager_id IS NULL');
-    managers.unshift({ id: null, manager_name: 'No Manager' });
-    
+    const [roles] = await promisePool.query("SELECT id, Title FROM Roles");
+    const [departments] = await promisePool.query(
+      "SELECT id, name FROM Departments"
+    );
+    const [managers] = await promisePool.query(
+      'SELECT id, CONCAT(first_name, " ", last_name) AS manager_name FROM Employee WHERE manager_id IS NULL'
+    );
+    managers.unshift({ id: null, manager_name: "No Manager" });
+
     // Prompt user for employee details
     const employeeData = await inquirer.prompt([
       {
@@ -142,13 +144,16 @@ async function addEmployee() {
         type: "list",
         name: "roleId",
         message: "Select employee's role:",
-        choices: roles.map(role => ({ name: role.Title, value: role.id }))
+        choices: roles.map((role) => ({ name: role.Title, value: role.id })),
       },
       {
         type: "list",
         name: "departmentId",
         message: "Select employee's department:",
-        choices: departments.map(department => ({ name: department.name, value: department.id }))
+        choices: departments.map((department) => ({
+          name: department.name,
+          value: department.id,
+        })),
       },
       {
         type: "input",
@@ -159,19 +164,34 @@ async function addEmployee() {
         type: "list",
         name: "managerId",
         message: "Select employee's manager:",
-        choices: managers.map(manager => ({ name: manager.manager_name, value: manager.id }))
+        choices: managers.map((manager) => ({
+          name: manager.manager_name,
+          value: manager.id,
+        })),
       },
     ]);
 
     // Add the employee to the database
     await promisePool.execute(
-      'INSERT INTO Employee (first_name, last_name, title_id, department_id, salary, manager_id) VALUES (?, ?, ?, ?, ?, ?)',
-      [employeeData.firstName, employeeData.lastName, employeeData.roleId, employeeData.departmentId, employeeData.salary, employeeData.managerId]
+      "INSERT INTO Employee (first_name, last_name, title_id, department_id, salary, manager_id) VALUES (?, ?, ?, ?, ?, ?)",
+      [
+        employeeData.firstName,
+        employeeData.lastName,
+        employeeData.roleId,
+        employeeData.departmentId,
+        employeeData.salary,
+        employeeData.managerId,
+      ]
     );
 
-    console.log('Employee', employeeData.firstName, employeeData.lastName, 'added successfully!');
+    console.log(
+      "Employee",
+      employeeData.firstName,
+      employeeData.lastName,
+      "added successfully!"
+    );
   } catch (error) {
-    console.error('Error adding employee:', error.message);
+    console.error("Error adding employee:", error.message);
   } finally {
     // Prompt user again or end the connection
     promptUser();
@@ -180,10 +200,18 @@ async function addEmployee() {
 async function updateEmployeeRole() {
   try {
     // Fetch employee list, roles, departments, and managers
-    const [employees] = await promisePool.query('SELECT id, CONCAT(first_name, " ", last_name) AS employee_name FROM Employee');
-    const [roles] = await promisePool.query('SELECT id, Title, department_id FROM Roles');
-    const [departments] = await promisePool.query('SELECT id, name FROM Departments');
-    const [managers] = await promisePool.query('SELECT id, CONCAT(first_name, " ", last_name) AS manager_name FROM Employee');
+    const [employees] = await promisePool.query(
+      'SELECT id, CONCAT(first_name, " ", last_name) AS employee_name FROM Employee'
+    );
+    const [roles] = await promisePool.query(
+      "SELECT id, Title, department_id FROM Roles"
+    );
+    const [departments] = await promisePool.query(
+      "SELECT id, name FROM Departments"
+    );
+    const [managers] = await promisePool.query(
+      'SELECT id, CONCAT(first_name, " ", last_name) AS manager_name FROM Employee'
+    );
 
     // Prompt user for employee, new role, new department, new salary, and new manager details
     const updateData = await inquirer.prompt([
@@ -191,19 +219,25 @@ async function updateEmployeeRole() {
         type: "list",
         name: "employeeId",
         message: "Select employee to update:",
-        choices: employees.map(employee => ({ name: employee.employee_name, value: employee.id })),
+        choices: employees.map((employee) => ({
+          name: employee.employee_name,
+          value: employee.id,
+        })),
       },
       {
         type: "list",
         name: "newRoleId",
         message: "Select employee's new role:",
-        choices: roles.map(role => ({ name: role.Title, value: role.id })),
+        choices: roles.map((role) => ({ name: role.Title, value: role.id })),
       },
       {
         type: "list",
         name: "newDepartmentId",
         message: "Select employee's new department:",
-        choices: departments.map(department => ({ name: department.name, value: department.id })),
+        choices: departments.map((department) => ({
+          name: department.name,
+          value: department.id,
+        })),
       },
       {
         type: "input",
@@ -214,19 +248,33 @@ async function updateEmployeeRole() {
         type: "list",
         name: "newManagerId",
         message: "Select employee's new manager:",
-        choices: [...managers.map(manager => ({ name: manager.manager_name, value: manager.id })), { name: "No Manager", value: null }],
+        choices: [
+          ...managers.map((manager) => ({
+            name: manager.manager_name,
+            value: manager.id,
+          })),
+          { name: "No Manager", value: null },
+        ],
       },
     ]);
 
     // Update the employee's role, department, salary, and manager in the database
     const [result] = await promisePool.query(
-      'UPDATE Employee SET title_id = ?, department_id = ?, salary = ?, manager_id = ? WHERE id = ?',
-      [updateData.newRoleId, updateData.newDepartmentId, updateData.newSalary, updateData.newManagerId, updateData.employeeId]
+      "UPDATE Employee SET title_id = ?, department_id = ?, salary = ?, manager_id = ? WHERE id = ?",
+      [
+        updateData.newRoleId,
+        updateData.newDepartmentId,
+        updateData.newSalary,
+        updateData.newManagerId,
+        updateData.employeeId,
+      ]
     );
 
-    console.log('Employee role, department, salary, and manager updated successfully!');
+    console.log(
+      "Employee role, department, salary, and manager updated successfully!"
+    );
   } catch (error) {
-    console.error('Error updating employee details:', error.message);
+    console.error("Error updating employee details:", error.message);
   } finally {
     // Prompt user again or end the connection
     promptUser();
@@ -236,7 +284,9 @@ async function updateEmployeeRole() {
 async function addRole() {
   try {
     // Fetch department list
-    const [departments] = await promisePool.query('SELECT id, name FROM Departments');
+    const [departments] = await promisePool.query(
+      "SELECT id, name FROM Departments"
+    );
 
     // Prompt user for new role details
     const roleData = await inquirer.prompt([
@@ -254,23 +304,26 @@ async function addRole() {
         type: "list",
         name: "departmentId",
         message: "Select the department for the new role:",
-        choices: departments.map(department => ({ name: department.name, value: department.id })),
+        choices: departments.map((department) => ({
+          name: department.name,
+          value: department.id,
+        })),
       },
     ]);
 
     // Insert the new role into the database
     const [result] = await promisePool.query(
-      'INSERT INTO Roles (Title, Salary, department_id) VALUES (?, ?, ?)',
+      "INSERT INTO Roles (Title, Salary, department_id) VALUES (?, ?, ?)",
       [roleData.title, roleData.salary, roleData.departmentId]
     );
 
     if (result.affectedRows > 0) {
-      console.log('New role added successfully!');
+      console.log("New role added successfully!");
     } else {
-      console.log('Role not added. Please check your input.');
+      console.log("Role not added. Please check your input.");
     }
   } catch (error) {
-    console.error('Error adding new role:', error.message);
+    console.error("Error adding new role:", error.message);
   } finally {
     // Prompt user again or end the connection
     promptUser();
@@ -289,11 +342,14 @@ async function addDepartment() {
     ]);
 
     // Insert the new department into the database
-    const [result] = await promisePool.query('INSERT INTO Departments (name) VALUES (?)', [departmentData.departmentName]);
+    const [result] = await promisePool.query(
+      "INSERT INTO Departments (name) VALUES (?)",
+      [departmentData.departmentName]
+    );
 
-    console.log('Department added successfully!');
+    console.log("Department added successfully!");
   } catch (error) {
-    console.error('Error adding department:', error.message);
+    console.error("Error adding department:", error.message);
   } finally {
     // Prompt user again or end the connection
     promptUser();
@@ -303,8 +359,3 @@ async function addDepartment() {
 // Initial call to start the application
 
 promptUser();
-
-
-
-
-
